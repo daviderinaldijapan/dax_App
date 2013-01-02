1f4d21e43aefd5189f25fce633fbd851fa204d98
davide <daviderinaldijapan@gmail.com>
2013-01-02 15:38:32 +0900

    Primo Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaxApp::Application.config.secret_token = '55bb1dc5d5e0c890999bee977ffd23f9cba9dc0718f801ae8e5bdd6e8f45c874985dc42746e799855ef10ef9e2d68944fc9f404c9acd305587fbbcd83a15bdbe'
